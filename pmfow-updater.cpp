ecbfee6f6394701d941d60d36f1fda46ae48cbf9
José Yañez <57007976+MasterJayanX@users.noreply.github.com>
2025-01-15 17:21:02 -0300

    1.3.0
    Added support for LTS, config.ini and fixed some minor issues

--- pmfow-updater.cpp
+++ pmfow-updater.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+bool configExists = true;
+
 class repo {
 public:
     repo(const string& programpath) {
@@ -48,6 +50,35 @@ private:
         }
         file.close();
     }
+};
+
+class Config {
+    // This class reads the config file
+public:
+    Config(string filename) {
+        // This function reads the config file
+        ifstream file(filename);
+        if(!file.is_open()) {
+            cout << "Error: Could not open file " << filename << endl;
+            configExists = false;
+            return;
+        }
+        string line;
+        while(getline(file, line)) {
+            if(line[0] != '#' && line[0] != ';' && line[0] != '`'){
+                int pos = line.find("=");
+                string key = line.substr(0, pos);
+                string value = line.substr(pos + 1);
+                data[key] = value;
+            }
+        }
+    }
+    string get(string key) {
+        // This function gets the value of a key
+        return data[key];
+    }
+private:
+    map<string, string> data;
 };
 
 string getWindowsVersion(int majorVersion, int minorVersion, int buildNumber){
@@ -108,12 +139,21 @@ string getArchitecture(){
     return arch;
 }
 
+string wchar_to_string(const wchar_t* wchar){
+    // This function converts a wchar_t to a string
+    string str;
+    for(int i = 0; wchar[i] != '\0'; i++){
+        str += static_cast<char>(wchar[i]);
+    }
+    return str;
+}
+
 string getEXEpath(){
-    // This function returns the path of pmfow's executable
-    char buffer[MAX_PATH];
-    GetModuleFileName(NULL, buffer, MAX_PATH);
-    string::size_type pos = string(buffer).find_last_of("\\/");
-    return string(buffer).substr(0, pos);
+    // This function returns the path of pmfow-updater's executable
+    wchar_t buffer[MAX_PATH];
+    GetModuleFileNameW(NULL, buffer, MAX_PATH);
+    wstring::size_type pos = wstring(buffer).find_last_of(L"\\/");
+    return wchar_to_string(buffer).substr(0, pos);
 }
 
 vector<string> repoDirectories(string programpath){
@@ -142,33 +182,77 @@ vector<string> repoDirectories(string programpath){
 int main(int argc, char** argv){
     OSVERSIONINFOW osv;
     osv.dwOSVersionInfoSize = sizeof(OSVERSIONINFOW);
-    osv.dwMajorVersion = 0, osv.dwMinorVersion = 0, osv.dwBuildNumber = 0; 
+    osv.dwMajorVersion = 0, osv.dwMinorVersion = 0, osv.dwBuildNumber = 0;
+    bool stable, lts;
     GetVersionExW(&osv);
     int majorVersion = osv.dwMajorVersion, minorVersion = osv.dwMinorVersion, build = osv.dwBuildNumber;
     string winver = getWindowsVersion(majorVersion, minorVersion, build), architecture = getArchitecture(), programpath = getEXEpath(), wget_exe, fullpath = programpath + "\\";
-    if(winver == "Windows XP" || winver == "Windows XP Professional x64/Windows Server 2003"){
-        wget_exe = "wget_xp";
+    string config_dir = programpath + "\\files\\config.ini";
+    Config config(config_dir);
+
+    if(config.get("winver") != "auto"){
+        winver = config.get("winver");
+        if(winver == "2000"){
+            osv.dwMajorVersion = 5, osv.dwMinorVersion = 0, osv.dwBuildNumber = 0;
+        }
+        else if(winver == "xp"){
+            osv.dwMajorVersion = 5, osv.dwMinorVersion = 1, osv.dwBuildNumber = 0;
+        }
+        else if(winver == "vista"){
+            osv.dwMajorVersion = 6, osv.dwMinorVersion = 0, osv.dwBuildNumber = 0;
+        }
+        else if(winver == "7"){
+            osv.dwMajorVersion = 6, osv.dwMinorVersion = 1, osv.dwBuildNumber = 0;
+        }
+        else if(winver == "8"){
+            osv.dwMajorVersion = 6, osv.dwMinorVersion = 3, osv.dwBuildNumber = 0;
+        }
+        else if(winver == "10"){
+            osv.dwMajorVersion = 10, osv.dwMinorVersion = 0, osv.dwBuildNumber = 0;
+        }
+        winver = getWindowsVersion(osv.dwMajorVersion, osv.dwMinorVersion, osv.dwBuildNumber);
     }
-    else if(winver == "Windows 2000"){
-        wget_exe = "wget_2k";
+    if(config.get("wget_version") == "auto"){
+        if(winver == "Windows XP" || winver == "Windows XP Professional x64/Windows Server 2003"){
+            wget_exe = "wget_xp";
+        }
+        else if(winver == "Windows 2000"){
+            wget_exe = "wget_2k";
+        }
+        else{
+            wget_exe = "wget";
+        }
     }
-    else{
-        wget_exe = "wget";
+    if(config.get("architecture") != "auto"){
+        architecture = config.get("architecture");
     }
+    if(config.get("check_lts") == "true"){
+        lts = true;
+    }
+    else if(config.get("check_lts") == "false"){
+        lts = false;
+    }
+    
     vector<string> directories = repoDirectories(programpath);
     if(osv.dwMajorVersion >= 5){
         // Windows 2000 or later
         repo r(programpath);
         string package;
-        bool stable = true;
+        stable = true, lts = false;
         if(argc > 1){
             if(string(argv[1]) == "-u" || string(argv[1]) == "--unstable"){
                 stable = false;
+            }
+            else if(string(argv[1]) == "-l" || string(argv[1]) == "--lts"){
+                lts = true;
             }
         }
         if(architecture == "x64"){
             if(stable){
                 package = r.update("pmfow64");
+            }
+            else if(lts){
+                package = r.update("pmfow-lts64");
             }
             else{
                 package = r.update("pmfow-unstable64");
@@ -177,6 +261,9 @@ int main(int argc, char** argv){
         else if(architecture == "x86"){
             if(stable){
                 package = r.update("pmfow32");
+            }
+            else if(lts){
+                package = r.update("pmfow-lts32");
             }
             else{
                 package = r.update("pmfow-unstable32");
@@ -187,7 +274,7 @@ int main(int argc, char** argv){
             return 0;
         }
         system(("del " + fullpath + "pmfow.exe").c_str());
-        system((wget_exe + " -O " + fullpath + "pmfow.exe " + package).c_str());
+        system((wget_exe + " -O " + fullpath + "pmfow.exe " + package + " -q --show-progress").c_str());
     }
     else{
         // Windows 98 or earlier
